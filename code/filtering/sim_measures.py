fb81091455f5ff07d6c5ee1f40401bc4dfad912c
Jonas Kaendler <jonas.pascal@web.de>
2020-06-07 17:32:08 +0000

    Sim measures: Changed old to run faster, added new measures and enhanced preprocessing steps added

--- code/filtering/sim_measures.py
+++ code/filtering/sim_measures.py
@@ -3,13 +3,19 @@
 
 #%%
 
+#import pandas as pd
+#import numpy as np
 import Levenshtein as lev
 import py_stringmatching as sm
 import re
 from datetime import datetime
+from scipy.spatial.distance import cosine
+from sklearn.feature_extraction.text import TfidfVectorizer
 #import nltk
 from nltk.corpus import stopwords
 sw = set(stopwords.words('english'))
+from nltk.stem import PorterStemmer 
+pstemmer = PorterStemmer()
 import logging
 logger = logging.getLogger(__name__)
 
@@ -24,68 +30,145 @@ def remove_paren(string):
     string = re.sub(r'\(.*\)','',string).strip()
     # before returning the string without parentheses also ensure no double whitespaces are in the string
     return re.sub(r'  ',' ',string).strip()
-
-# remove stopwords from string
-def remove_sw(string,output_list=False):
-    string = str(string).lower().split()
-    if(output_list):
-        return [w for w in string if not w in sw]
+        
+def remove_sw(string,output_list=False,stem=False):
+    string_lst = re.sub('[^A-Za-z0-9\s\t\n]+', '', str(string).lower().strip()).split()
+    if(stem):
+        if(output_list):
+            return [pstemmer.stem(w) for w in string_lst if not w in sw]
+        else:
+            return ' '.join([pstemmer.stem(w) for w in string_lst if not w in sw]).strip()
     else:
-        return ' '.join([w for w in string if not w in sw]).strip()
+        if(output_list):
+            return [w for w in string_lst if not w in sw]
+        else:
+            return ' '.join([w for w in string_lst if not w in sw]).strip()
 
 #%% String Similarity Measures
 
+# this function is quite slow because it needs to tokenize the words in 3-grams first
 def jac_q3_sim(str1,str2):
     try:
-        # lower case the two input strings
-        str1 = str(str1).lower().strip()
-        str2 = str(str2).lower().strip()
+        # not needed as we already casted all to string and
+        # lower cased and stripped all values before handing it over
+        #str1 = str(str1).lower().strip()
+        #str2 = str(str2).lower().strip()
         # assign a sim score of -1 when one of them is null
-        if (str1 == '' or str2 == '' or str1 == 'nan' or str2 == 'nan'):
+        if (str1 == 'nan' or str2 == 'nan' or str1 == '' or str2 == ''):
             return -1
         else:
             q3_tok = sm.QgramTokenizer(qval=3,return_set=True)
             jac = sm.Jaccard()
             return jac.get_raw_score(q3_tok.tokenize(str1),q3_tok.tokenize(str2))
     except:
-        logger.warning('No Strings at least one of the two str1: {} and str2: {} or could not be tokenized, hence -1 assigend'.format(str1,str2))
+        logger.warning('Issue with Jaccard_q3_Sim, hence -1 assigned')
         return -1
 
 
 #%%
 
-def jac_an_sim(str1,str2):
+# TOO SLOW!!!
+#def jac_an_sim(str1,str2):
+#    try:
+#        # lower case the two input strings
+#        str1 = str(str1).lower().strip()
+#        str2 = str(str2).lower().strip()
+#        # assign a sim score of -1 when one of them is null
+#        if (str1 == '' or str2 == '' or str1 == 'nan' or str2 == 'nan'):
+#            return -1
+#        else:
+#            an_tok = sm.AlphanumericTokenizer(return_set=True)
+#            jac = sm.Jaccard()
+#            return jac.get_raw_score(an_tok.tokenize(str1),an_tok.tokenize(str2))
+#    except:
+#        logger.warning('No Strings at least one of the two str1: {} and str2: {} or could not be tokenized, hence -1 assigend'.format(str1,str2))
+#        return -1
+
+
+#%%
+        
+def jac_an_sim(str1, str2):
     try:
-        # lower case the two input strings
-        str1 = str(str1).lower().strip()
-        str2 = str(str2).lower().strip()
-        # assign a sim score of -1 when one of them is null
-        if (str1 == '' or str2 == '' or str1 == 'nan' or str2 == 'nan'):
-            return -1
+        # not needed as we already casted all to string and
+        # lower cased and stripped all values before handing it over
+        #str1 = str(str1).lower().strip()
+        #str2 = str(str2).lower().strip()
+        # create (basically tokens sperated by whitespace) token sets (need to be set for Jaccard)
+        a = set(str1.split())
+        b = set(str2.split())
+        # calculate intersection of both sets
+        c = a.intersection(b)
+        if (str1 == 'nan' or str2 == 'nan' or str1 == '' or str2 == ''):
+            return -1.0
         else:
-            an_tok = sm.AlphanumericTokenizer(return_set=True)
-            jac = sm.Jaccard()
-            return jac.get_raw_score(an_tok.tokenize(str1),an_tok.tokenize(str2))
+            return float(len(c)) / float(len(a) + len(b) - len(c))
     except:
-        logger.warning('No Strings at least one of the two str1: {} and str2: {} or could not be tokenized, hence -1 assigend'.format(str1,str2))
+        logger.warning('Issue with Jaccard_an_Sim, hence -1 assigned')
+        return -1
+    
+#%%
+def relaxed_jaccard_sim(str1, str2):
+    try:
+        # not needed as we already casted all to string and
+        # lower cased and stripped all values before handing it over
+        #str1 = str(str1).lower().strip()
+        #str2 = str(str2).lower().strip()
+        if (str1 == 'nan' or str2 == 'nan' or str1 == '' or str2 == ''):
+            return -1
+        a = set(str1.split())
+        b = set(str2.split())
+        c = []
+        for a_ in a:
+            for b_ in b:
+                if lev_sim(a_, b_) > 0.7:
+                    c.append(a_)
+    
+        intersection = len(c)
+        min_length = min(len(a), len(b))
+        if intersection > min_length:
+            intersection = min_length
+        return float(intersection) / float(len(a) + len(b) - intersection)
+    except:
+        logger.warning('Issue with Relaxed Jaccard Sim, hence -1 assigned')
         return -1
 
+#%%
+    
+def containment_sim(str1, str2):
+    try:
+        # not needed as we already casted all to string and
+        # lower cased and stripped all values before handing it over
+        # a = set(str(str1).lower().split())
+        # b = set(str(str2).lower().split())
+        a = set(str1.split())
+        b = set(str2.split())
+        c = a.intersection(b)
+        if (str1 == 'nan' or str2 == 'nan' or str1 == '' or str2 == ''):
+            return -1
+        elif (len(a) == 0 or len(b) == 0):
+            return -1
+        else:
+            return float(len(c)) / float(min(len(a), len(b)))
+    except:
+        logger.warning('Issue with Containment Sim, hence -1 assigned')
+        return -1
 
 #%%
 
 def lev_sim(str1,str2):
     try:
-        # lower case the two input strings
-        str1 = str(str1).lower().strip()
-        str2 = str(str2).lower().strip()
+        # not needed as we already casted all to string and
+        # lower cased and stripped all values before handing it over
+        # str1 = str(str1).lower().strip()
+        # str2 = str(str2).lower().strip()
         # assign a sim score of -1 when one of them is null
-        if (str1 == '' or str2 == '' or str1 == 'nan' or str2 == 'nan'):
+        if (str1 == 'nan' or str2 == 'nan' or str1 == '' or str2 == ''):
             return -1
         else:
             max_len = max(len(str1), len(str2))
             return 1 - (lev.distance(str1,str2) / max_len)
     except:
-        logger.warning('No Strings at least one of the two str1: {} and str2: {}, hence -1 assigend'.format(str1,str2))
+        logger.warning('Issue with Levenshtein, hence -1 assigned')
         return -1
 
 
@@ -93,18 +176,19 @@ def lev_sim(str1,str2):
 
 def exact_sim(str1,str2):
     try:
-        # lower case the two input strings
-        str1 = str(str1).lower().strip()
-        str2 = str(str2).lower().strip()
+        # not needed as we already casted all to string and
+        # lower cased and stripped all values before handing it over
+        # str1 = str(str1).lower().strip()
+        # str2 = str(str2).lower().strip()
         # assign a sim score of -1 when one of them is null
-        if (str1 == '' or str2 == '' or str1 == 'nan' or str2 == 'nan'):
+        if (str1 == 'nan' or str2 == 'nan' or str1 == '' or str2 == ''):
             return -1
         elif (str1 == str2):
             return 1
         else:
             return 0
     except:
-        logger.warning('No Strings at least one of the two str1: {} and str2: {}, hence -1 assigend'.format(str1,str2))
+        logger.warning('Issue with exact_sim, hence -1 assigend')
         return -1        
 
 
@@ -236,17 +320,38 @@ def days_diff(str1,str2):
     
 #%%
 
-def all_missing(str1,str2):
-    try:
-        # lower case the two input strings
-        str1 = str(str1).lower().strip()
-        str2 = str(str2).lower().strip()
-        # assign a sim score of 1 when both of them are null
-        if ((str1 == '' and str2 == '') or (str1 == 'nan' and str2 == 'nan')):
-            return 1
-        else:
-            return 0
-    except:
-        logger.warning('No String at least one of the two str1: {} and str2: {}, hence -1 assigend'.format(str1,str2))
-        return -1
+# commented out all_missing as this feature could be misleading
+#def all_missing(str1,str2):
+#    try:
+#        # lower case the two input strings
+#        str1 = str(str1).lower().strip()
+#        str2 = str(str2).lower().strip()
+#        # assign a sim score of 1 when both of them are null
+#        if ((str1 == '' and str2 == '') or (str1 == 'nan' and str2 == 'nan')):
+#            return 1
+#        else:
+#            return 0
+#    except:
+#        logger.warning('No String at least one of the two str1: {} and str2: {}, hence -1 assigend'.format(str1,str2))
+#        return -1
 
+#%%  
+
+def calculate_cosine_tfidf(df):   
+    if(len(df.columns) != 2):
+        raise ValueError('The function calculate_tfidf needs the dataframe with two columns\
+                         where each contain the data of the long string attribute from one source')
+    # commented out because pre-processing not needed. Was already applied before handing it over, to make it faster.
+#    df['concat'] = df.apply(lambda row: "{} {}".format(re.sub('[^A-Za-z0-9\s\t\n]+', '', str(row[0]).lower().strip()),
+#                                                       re.sub('[^A-Za-z0-9\s\t\n]+', '', str(row[1]).lower().strip())),axis=1)
+    
+    df['concat'] = df.apply(lambda row: "{} {}".format(row[0],row[1]),axis=1)
+    
+    clf = TfidfVectorizer()
+    clf.fit(df['concat'])
+    
+    tfidf_series1 = clf.transform(df.iloc[:,0]).todense()
+    tfidf_series2 = clf.transform(df.iloc[:,1]).todense()
+    
+    cosine_tfidf_sim = [1 - cosine(tfidf_series1[x],tfidf_series2[x]) for x in range(df.shape[0])]
+    return cosine_tfidf_sim
